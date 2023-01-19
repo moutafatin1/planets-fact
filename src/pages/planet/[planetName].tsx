ebdfc8a56a84f06e6703fd3159414eb4c60594c7
Oussama Moutafatin <moutafatin3@gmail.com>
2023-01-19 10:00:41 +0100

    assets and styles guide

---
+++ src/pages/planet/[planetName].tsx
@@ -0,0 +1,3 @@
+export const PlantDetailsPage = () => {
+  return <div className="">PlantDetailsPage</div>;
+};
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: "#070724",
+        arsenic: "#38384F",
+        silver: "#838391",
+      },
+      backgroundImage: {
+        main: "url('/background-stars.svg')",
+      },
+    },
   },
   plugins: [],
 };
